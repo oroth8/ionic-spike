5895376bd5ec3cb527e44091e48dab36cb405a4b
Owen Roth <69156111+oroth8@users.noreply.github.com>
2024-02-06 15:02:17 -0600

    take-photos

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -16,8 +16,10 @@ import {
 import { camera, trash, close } from "ionicons/icons";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab2.css";
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
 
 const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -26,6 +28,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
