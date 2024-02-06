ba1e3a8f7f94b532d340b2e8562ccc9684c07f12
Owen Roth <69156111+oroth8@users.noreply.github.com>
2024-02-06 14:53:58 -0600

    init ionic files, update tab 2

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,39 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from "@ionic/react";
+import { camera, trash, close } from "ionicons/icons";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
